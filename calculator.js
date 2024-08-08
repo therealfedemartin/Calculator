be1ad2045df31a115518c7f5d44672f1b70a71e8
Therealfedemartin <125780104+therealfedemartin@users.noreply.github.com>
2024-08-08 16:49:47 -0300

    solved most problems

--- calculator.js
+++ calculator.js
@@ -18,7 +18,7 @@ function divide (a, b) {
 }
 //**** CREATE FUNCTION THAT CALLS CALCULATION FUNCTIONS ****
 
-function operate (numberOne, operator, numberTwo) {
+function operate(numberOne, operator, numberTwo) {
     switch (operator) {
         case "+":
             return add(numberOne, numberTwo);  
@@ -34,9 +34,10 @@ function operate (numberOne, operator, numberTwo) {
 //**** Variables defined  ****
 
 let operators = "";
-let secondNumber = "";
+let secondNumber = 0;
 let pressed = "";
-let firstNumber = "";
+let firstNumber = 0;
+let toDecimal = false;
 
 //**** BTNS defined ****
 
@@ -44,19 +45,20 @@ const display = document.querySelector("#display");
 display.textContent = "0";
 
 const numb = document.querySelectorAll('.numb');
+
 for (const btn of numb) {
     btn.addEventListener('click', () => {
         if (display.textContent.length < 16) {
             if (pressed === "") {
-                if (display.textContent === "0") {
+                if (display.textContent === "0" && !toDecimal) {
                     display.textContent = btn.textContent;
                 } else {
                     display.textContent += btn.textContent;
                 }
             } else {
-                display.textContent = "";
                 pressed = "";
                 display.textContent = btn.textContent;
+                toDecimal = false;
             }
         }
         addBTN.style.opacity = "1";
@@ -66,17 +68,23 @@ for (const btn of numb) {
     });
 }
 
+
+
 const zero = document.querySelector(".zero");
 zero.addEventListener('click', () => {
     if (display.textContent.length < 16) {
-        if (display.textContent === "0") {
-            } else if (pressed  != "" && display.textContent != "0") {
+        if (display.textContent === "0" && !toDecimal) {
+            } else if (pressed  !== "" && display.textContent !== "0") {
                 display.textContent = zero.textContent;
             } else {
                 display.textContent += zero.textContent;
             }
     }
     addBTN.style.opacity = "1";
+    subtractBTN.style.opacity = "1";
+    multiplyBTN.style.opacity = "1";
+    divideBTN.style.opacity = "1";
+    toDecimal = false;
 });
 
 const clear = document.querySelector(".clear");
@@ -84,111 +92,117 @@ clear.addEventListener('click', () => {
     if (display.textContent === "0") {
     } else {
         display.textContent = "0";
-        firstNumber = "";
-        secondNumber = "";
+        firstNumber = 0;
+        secondNumber = 0;
         operators = "";
+        toDecimal = false;
     }
     addBTN.style.opacity = "1";
     subtractBTN.style.opacity = "1";
     multiplyBTN.style.opacity = "1";
-    divideBTN.style.opacity = "1";
+    divideBTN.style.opacity = "1";  
 });
 
 const addBTN = document.querySelector(".addBTN");
 addBTN.addEventListener('click', () => {
     addBTN.style.opacity = "0.5";
-    firstNumber = Number(display.textContent);
+    firstNumber = parseFloat(display.textContent);
     operators = "+";
     pressed = "Algo";
-    
     subtractBTN.style.opacity = "1";
     multiplyBTN.style.opacity = "1";
     divideBTN.style.opacity = "1";
+    toDecimal = false;
+
 });
 
 const subtractBTN = document.querySelector(".subtractBTN");
 subtractBTN.addEventListener('click', () => {
     subtractBTN.style.opacity = "0.5";
-    firstNumber = Number(display.textContent);
+    firstNumber = parseFloat(display.textContent);
     operators = "-";
     pressed = "Algo";
     addBTN.style.opacity = "1";
     multiplyBTN.style.opacity = "1";
     divideBTN.style.opacity = "1";
+    toDecimal = false;
+
 });
 
 const multiplyBTN = document.querySelector(".multiplyBTN");
 multiplyBTN.addEventListener('click', () => {
     multiplyBTN.style.opacity = "0.5";
-    firstNumber = Number(display.textContent);
+    firstNumber = parseFloat(display.textContent);
     operators = "*";
     pressed = "Algo";
     addBTN.style.opacity = "1";
     subtractBTN.style.opacity = "1";
     divideBTN.style.opacity = "1";
+    toDecimal = false   ;
+
 });
 
 const divideBTN = document.querySelector(".divideBTN");
 divideBTN.addEventListener('click', () => {
     divideBTN.style.opacity = "0.5";
-    firstNumber = Number(display.textContent);
+    firstNumber = parseFloat(display.textContent);
     operators = "/";
     pressed = "Algo";
     addBTN.style.opacity = "1";
     subtractBTN.style.opacity = "1";
     multiplyBTN.style.opacity = "1";
+    toDecimal = false;
+
 });
 
 const equal = document.querySelector(".equal");
 equal.addEventListener('click', () => {
     if (operators === "/" && display.textContent === "0") {
-        display.textContent = "∞ UOPS! ∞"
-        firstNumber = "";
-        secondNumber = "";
+        display.textContent = "∞ UOPS! ∞";
+        firstNumber = 0;
+        secondNumber = 0;
         operators = "";
+        toDecimal = false;
     } else {
-        if (operators != "" ) {
-            secondNumber = Number(display.textContent);
+        if (operators !== "" ) {
+            secondNumber = parseFloat(display.textContent);
             let result = operate(firstNumber, operators, secondNumber);
-            if ((result % 1) != 0) {
-                let withDecimal = Number(result).toFixed(2)
-                display.textContent = withDecimal;
-            } else {
-                display.textContent = result;
-            }
-            
-            firstNumber = "";
-            secondNumber = "";
+            display.textContent = result.toFixed(2);
+            firstNumber = result;
+            secondNumber = 0;
             operators = "";
+            toDecimal = false;
+
         }
     }
 });
 
 const percentage = document.querySelector(".percentage");
 percentage.addEventListener('click', () => {
-    if (display.textContent != "0") {
+    if (display.textContent !== "0") {
         if (display.textContent.length < 6) {
-            let toDecimal = Number(display.textContent) / 100;
-            display.textContent = toDecimal;
+            let divideH = parseFloat(display.textContent) / 100;
+            display.textContent = divideH;
         }
     }
 });
 
 const sign = document.querySelector(".sign");
 sign.addEventListener('click', () => {
-    if (display.textContent != "0") {
-        let toDecimal = Number(display.textContent) * -1;
-        display.textContent = toDecimal;
+    if (display.textContent !== "0") {
+        let changeSign = parseFloat(display.textContent) * -1;
+        display.textContent = changeSign.toFixed(2);
     }
 });
 
 const dot = document.querySelector(".dot");
 dot.addEventListener('click', () => {
-let arr = display.textContent;
-let newArr = arr.split("");
-    if (newArr.includes(".") === true) {
-
-    } else {
+  if (!display.textContent.includes(".") && pressed === "") {
     display.textContent += dot.textContent;
-    }
+    toDecimal = true;
+  } else if (pressed !== "") {
+    display.textContent = "0.";
+    toDecimal = true;
+    pressed = "";
+  }
 });
